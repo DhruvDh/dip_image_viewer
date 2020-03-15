433320287d53bf7c2b492ef4a97b7f209ab14fba
Dhruv Dhamani <dhruv.dhamani@gmail.com>
2020-03-15 10:05:32 -0400

    support for gray?

--- docs/sketch.js
+++ docs/sketch.js
@@ -52,6 +52,12 @@ function parseAndDraw() {
     document.getElementById("logContainer").className = "message is-dark";
     document.getElementById("hint").innerText = "Log";
     document.getElementById("errorLog").innerText = "";
+    
+    document.getElementById("histograms").innerHTML = `
+        <canvas id="histogram-red" class="container"></canvas>
+        <canvas id="histogram-green" class="container"></canvas>
+        <canvas id="histogram-blue" class="container"></canvas>
+    `;
 
     const fileInput = document.getElementById("fileInput");
 
@@ -64,7 +70,7 @@ function parseAndDraw() {
 
         assert(text.length <= 3, "You've entered less than or equal to 3 lines.");
 
-        assert(text[0] != "RGB", 'The first line of the file is not "RGB".');
+        assert(text[0] != "RGB" && text[0] != "G", 'The first line of the file is not "RGB or G".');
 
         assert(
             isNaN(text[1]),
@@ -96,74 +102,103 @@ function parseAndDraw() {
         let onlyRed = [];
         let onlyGreen = [];
         let onlyBlue = [];
+        let onlyGray = []
 
-        for (let i = 3; i < height * width + 3; i++) {
-            let colors = text[i].split(" ");
-
-            assert(
-                colors.length != 3,
-                `I can't find 3 colors for pixel no. ${i - 3}.`
-            );
-            let red = parseInt(colors[0]);
-            let green = parseInt(colors[1]);
-            let blue = parseInt(colors[2]);
-
-            assert(
-                isNaN(red),
-                `I cannot parse the red component for pixel no. ${i - 3} as a number.`
-            );
-
-            assert(
-                red < 0 && red > 255,
-                `Red component of pixel no. ${i - 3} is not between 0 and 255.`
-            );
-            assert(
-                isNaN(green),
-                `I cannot parse the green component for pixel no. ${i - 3} as a number.`
-            );
-
-            assert(
-                green < 0 && green > 255,
-                `Green component of pixel no. ${i - 3} is not between 0 and 255.`
-            );
-            assert(
-                isNaN(blue),
-                `I cannot parse the blue component for pixel no. ${i - 3} as a number.`
-            );
-
-            assert(
-                blue < 0 && blue > 255,
-                `Blue component of pixel no. ${i - 3} is not between 0 and 255.`
-            );
-
-            img.push(red);
-            img.push(green);
-            img.push(blue);
-            img.push(255);
-
-            onlyRed.push(red);
-            onlyGreen.push(green);
-            onlyBlue.push(blue);
+        if (text[0] == "G") {
+            for (let i = 3; i < height * width + 3; i++) {
+                let colors = text[i].split(" ");
+    
+                assert(
+                    colors.length != 1,
+                    `Number of colors at ${i - 3} is not one.`
+                );
+                let gray = parseInt(colors[0]);
+    
+                assert(
+                    isNaN(gray),
+                    `I cannot parse the gray component for pixel no. ${i - 3} as a number.`
+                );
+    
+                assert(
+                    gray < 0 && gray > 255,
+                    `Red component of pixel no. ${i - 3} is not between 0 and 255.`
+                );
+                   
+                img.push(gray);
+                img.push(gray);
+                img.push(gray);
+                img.push(255);
+    
+                onlyGray.push(gray)
+            }
+        } else {
+            for (let i = 3; i < height * width + 3; i++) {
+                let colors = text[i].split(" ");
+    
+                assert(
+                    colors.length != 3,
+                    `I can't find 3 colors for pixel no. ${i - 3}.`
+                );
+                let red = parseInt(colors[0]);
+                let green = parseInt(colors[1]);
+                let blue = parseInt(colors[2]);
+    
+                assert(
+                    isNaN(red),
+                    `I cannot parse the red component for pixel no. ${i - 3} as a number.`
+                );
+    
+                assert(
+                    red < 0 && red > 255,
+                    `Red component of pixel no. ${i - 3} is not between 0 and 255.`
+                );
+                assert(
+                    isNaN(green),
+                    `I cannot parse the green component for pixel no. ${i - 3} as a number.`
+                );
+    
+                assert(
+                    green < 0 && green > 255,
+                    `Green component of pixel no. ${i - 3} is not between 0 and 255.`
+                );
+                assert(
+                    isNaN(blue),
+                    `I cannot parse the blue component for pixel no. ${i - 3} as a number.`
+                );
+    
+                assert(
+                    blue < 0 && blue > 255,
+                    `Blue component of pixel no. ${i - 3} is not between 0 and 255.`
+                );
+    
+                img.push(red);
+                img.push(green);
+                img.push(blue);
+                img.push(255);
+    
+                onlyRed.push(red);
+                onlyGreen.push(green);
+                onlyBlue.push(blue);
+            }
         }
 
         img = Uint8ClampedArray.from(img);
-        onlyRed = Uint8ClampedArray.from(onlyRed);
-        onlyGreen = Uint8ClampedArray.from(onlyGreen);
-        onlyBlue = Uint8ClampedArray.from(onlyBlue);
+
+        onlyRed = onlyRed.length > 0 ? Uint8ClampedArray.from(onlyRed) : undefined;
+        onlyGreen = onlyGreen.length > 0 ? Uint8ClampedArray.from(onlyGreen) : undefined;
+        onlyBlue = onlyBlue.length > 0 ? Uint8ClampedArray.from(onlyBlue) : undefined;
+        onlyGray = onlyGray.length > 0 ? Uint8ClampedArray.from(onlyGray) : undefined;
 
         doTheThing(img, width, height);
-        doTheHistogramThing(onlyRed, onlyBlue, onlyGreen);
+        doTheHistogramThing(onlyGray, onlyRed, onlyGreen, onlyBlue, width * height);
         document.getElementById("logContainer").className = "message is-primary";
         document.getElementById("hint").innerText = "I think it worked.";
     };
 }
 
-function doTheHistogramThing(onlyRed, onlyBlue, onlyGreen) {
-    var redctx = document.getElementById("histogram-red").getContext("2d");
-    var greenctx = document.getElementById("histogram-green").getContext("2d");
-    var bluectx = document.getElementById("histogram-blue").getContext("2d");
+function doTheHistogramThing(onlyGray, onlyRed, onlyGreen, onlyBlue, numPixels) {
     Chart.defaults.global.datasets.scatter.showLine = true;
-
+    
     const count = arr =>
         arr.reduce((prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev), {});
     const getData = arr => {
@@ -185,6 +220,10 @@ function doTheHistogramThing(onlyRed, onlyBlue, onlyGreen) {
             }
         }
 
+        data.forEach(point => {
+            point.y = (point.y / numPixels)
+        });
+
         return data;
     };
 
@@ -205,49 +244,73 @@ function doTheHistogramThing(onlyRed, onlyBlue, onlyGreen) {
         }
     }
 
-    var redChart = new Chart(redctx, {
-        type: 'scatter',
-        data: {
-            datasets: [{
-                label: 'Red',
-                data: getData(onlyRed),
-                backgroundColor: ['rgba(255, 99, 132, 0.2)'],
-                borderWidth: 1,
-                borderColor: ['rgb(255, 0, 0)']
-            }]
-        },
-        options: chartOptions
-    });
+    if (onlyGray != null) {
+        var grayctx = document.getElementById("histogram-red").getContext("2d");
 
-    var greenChart = new Chart(greenctx, {
-        type: 'scatter',
-        data: {
-            datasets: [{
-                label: 'Green',
-                data: getData(onlyGreen),
-                backgroundColor: ['rgba(75, 192, 192, 0.2)'],
-                borderWidth: 1,
-                borderColor: ['rgb(0, 255, 0)']
-            }]
-        },
-        options: chartOptions
-    });
+        var grayChart = new Chart(grayctx, {
+            type: 'scatter',
+            data: {
+                datasets: [{
+                    label: 'Gray',
+                    data: getData(onlyGray),
+                    backgroundColor: ['rgba(128, 128, 128, 0.2)'],
+                    borderWidth: 1,
+                    borderColor: ['rgb(128, 128, 128)']
+                }]
+            },
+            options: chartOptions
+        });
 
-    var blueChart = new Chart(bluectx, {
-        type: 'scatter',
-        data: {
-            datasets: [{
-                label: 'Blue',
-                data: getData(onlyBlue),
-                backgroundColor: ['rgba(54, 162, 235, 0.2)'],
-                borderWidth: 1,
-                borderColor: ['rgb(0, 0, 255)']
-            }]
-        },
-        options: chartOptions
-    });
-
-    return redChart, greenChart, blueChart;
+        return grayChart, undefined, undefined;
+    } else {
+        var redctx = document.getElementById("histogram-red").getContext("2d");
+        var greenctx = document.getElementById("histogram-green").getContext("2d");
+        var bluectx = document.getElementById("histogram-blue").getContext("2d");
+        
+        var redChart = new Chart(redctx, {
+            type: 'scatter',
+            data: {
+                datasets: [{
+                    label: 'Red',
+                    data: getData(onlyRed),
+                    backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+                    borderWidth: 1,
+                    borderColor: ['rgb(255, 0, 0)']
+                }]
+            },
+            options: chartOptions
+        });
+    
+        var greenChart = new Chart(greenctx, {
+            type: 'scatter',
+            data: {
+                datasets: [{
+                    label: 'Green',
+                    data: getData(onlyGreen),
+                    backgroundColor: ['rgba(75, 192, 192, 0.2)'],
+                    borderWidth: 1,
+                    borderColor: ['rgb(0, 255, 0)']
+                }]
+            },
+            options: chartOptions
+        });
+    
+        var blueChart = new Chart(bluectx, {
+            type: 'scatter',
+            data: {
+                datasets: [{
+                    label: 'Blue',
+                    data: getData(onlyBlue),
+                    backgroundColor: ['rgba(54, 162, 235, 0.2)'],
+                    borderWidth: 1,
+                    borderColor: ['rgb(0, 0, 255)']
+                }]
+            },
+            options: chartOptions
+        });
+    
+        return redChart, greenChart, blueChart;
+    }
 }
 
 function doTheThing(imgArray, width, height) {
